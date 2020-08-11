35ad778be4d7fc8d9b58ec79e41151dc41b36c9c
Jokk3r <jokzer43@gmail.com>
2020-08-11 14:01:49 +0200

    Updates

---
+++ precache-manifest.4056906119892bb3c864ec86451f1b21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7edc126c1d3c0d1ca84be2db8532745",
+    "url": "/ToDo-other-version-test-/index.html"
+  },
+  {
+    "revision": "5f4eda2a4f259c795aee",
+    "url": "/ToDo-other-version-test-/static/css/main.424edbec.chunk.css"
+  },
+  {
+    "revision": "2b7102446546307994dc",
+    "url": "/ToDo-other-version-test-/static/js/2.c881ae52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDo-other-version-test-/static/js/2.c881ae52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f4eda2a4f259c795aee",
+    "url": "/ToDo-other-version-test-/static/js/main.10abee1e.chunk.js"
+  },
+  {
+    "revision": "4732bda0a1933823f21b",
+    "url": "/ToDo-other-version-test-/static/js/runtime-main.dc5e24d8.js"
+  }
+]);
